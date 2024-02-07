a89379f5fbae36001dc67a5532a9058497fb82f2
manjunath.ab <manjunath.ab@northeastern.edu>
2024-02-07 17:46:17 -0500

    initialized dagster

---
+++ dagster_configuration/dagster_ai4mentalhealth/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dagster_ai4mentalhealth",
+    packages=find_packages(exclude=["dagster_ai4mentalhealth_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-cloud",
+        "pandas",
+        "langchain-openai",
+        "langchain-community",
+        "langchain",
+        "selenium"
+
+
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
