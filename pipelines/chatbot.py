18c924b63e6085f677c5b60bf6a6434549831b12
manjunath.ab <manjunath.ab@northeastern.edu>
2024-02-17 19:30:04 -0500

    chat functionality

--- pipelines/chatbot.py
+++ pipelines/chatbot.py
@@ -30,7 +30,7 @@ snowflake_loader = SnowflakeLoader(
     role=snowflake_role
 )
 snowflake_documents = snowflake_loader.load()
-embeddings = HuggingFaceEmbeddings()
+
 
 
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=4)
@@ -38,6 +38,77 @@ docs = text_splitter.split_documents(snowflake_documents)
 db = Chroma.from_documents(docs, OpenAIEmbeddings())
 
 
-query = "Tell me about depression. Talk about the symptoms and coping mechanisms like a friend"
-docs = db.similarity_search(query)
-print(docs[0].page_content)
+query = "Tell me about bipolar disorder and how to cope with it"
+result = db.similarity_search(query)
+# k is the number of chunks to retrieve
+retriever = db.as_retriever(k=4)
+
+docs = retriever.invoke("how can i battle depression?")
+
+"""
+CHATBOT SECTION
+"""
+from langchain_openai import ChatOpenAI
+
+chat = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0.2)
+
+#from langchain_core.messages import HumanMessage
+
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+chat = ChatOpenAI(model="gpt-3.5-turbo-1106")
+
+question_answering_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "Imagine you are a therapist, talk to the user like a friend who understands their problem and keep the answer short..end with a question:\n\n{context}",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+document_chain = create_stuff_documents_chain(chat, question_answering_prompt)
+
+"""
+document chain
+"""
+from langchain.memory import ChatMessageHistory
+
+demo_ephemeral_chat_history = ChatMessageHistory()
+
+demo_ephemeral_chat_history.add_user_message("how can I battle depression?")
+
+document_chain.invoke(
+    {
+        "messages": demo_ephemeral_chat_history.messages,
+        "context": docs,
+    }
+)
+
+"""
+retreival chain
+"""
+from typing import Dict
+
+from langchain_core.runnables import RunnablePassthrough
+
+
+def parse_retriever_input(params: Dict):
+    return params["messages"][-1].content
+
+
+retrieval_chain = RunnablePassthrough.assign(
+    context=parse_retriever_input | retriever,
+).assign(
+    answer=document_chain,
+)
+
+response = retrieval_chain.invoke(
+    {
+        "messages": demo_ephemeral_chat_history.messages,
+    }
+)
+
+response
