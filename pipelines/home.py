f815cf5b8987cbf9134a76e1e7461505e3a055dd
SivaranjaniSuresh <s.si@northeastern.edu>
2024-02-27 23:19:31 -0500

    Adding main function

--- pipelines/home.py
+++ pipelines/home.py
@@ -2,6 +2,8 @@ import streamlit as st
 #from  login import login, sign_up
 import login
 import agent_chat_prod
+import dashboard
+import pandas as pd
 
 def main():
     
@@ -9,7 +11,7 @@ def main():
     if 'is_logged_in' not in st.session_state:
         st.session_state.is_logged_in = False
 
-    option = st.sidebar.selectbox("Menu", ["Chat", "Sign Up", "Login"])
+    option = st.sidebar.selectbox("Menu", ["Chat", "Sign Up", "Login", "Dashboard"])
 
     if option == "Chat":
         if st.session_state.is_logged_in:
@@ -22,6 +24,11 @@ def main():
         st.session_state.is_logged_in = login.main()
         st.session_state.is_logged_in=True
 
+    elif option == "Dashboard":  # Add this new condition
+        if st.session_state.is_logged_in:
+            dashboard.main()  # Call the main function of the therapist dashboard
+        else:
+            st.write("Please log in to access the therapist dashboard.")
 
     else:
         st.session_state.is_logged_in = login.main()
