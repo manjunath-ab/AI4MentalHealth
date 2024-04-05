b6d460788ffe6d13be4bab76f5f4f9445be7040e
manjunath.ab <manjunath.ab@northeastern.edu>
2024-04-05 17:35:42 -0400

    last modification before testing

--- pipelines/home.py
+++ pipelines/home.py
@@ -1,5 +1,26 @@
 import streamlit as st
-#from  login import login, sign_up
+
+# Set page configuration
+st.set_page_config(page_title="Mental Health Chat", page_icon=":speech_balloon:")
+
+# Set theme colors
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: linear-gradient(45deg, #FF7E5F, #1A4876);
+        color: white;
+    }
+    .sidebar .sidebar-content {
+        background: linear-gradient(45deg, #1A4876, #FF7E5F);
+        color: white;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# from  login import login, sign_up
 import login
 import agent_chat_prod
 import therapy
@@ -12,42 +33,51 @@ def main():
         st.session_state.is_logged_in = False
 
     option = st.sidebar.selectbox("Menu", ["Chat", "Sign Up", "Login","Therapist View","Admin Dashboard"])
-    st.image("zen.jpg", width=300)
+    st.image("ai4mh.jpeg", width=300)
 
     # Upload an image on the sidebar
-    avatar_image = "avatar.jpg"
-
+    avatar_image = "dog2.jpeg"
+    
     # Check if an image file is uploaded
     if avatar_image is not None:
         # Display the uploaded image on the sidebar
         st.sidebar.image(avatar_image, use_column_width=True)
-        #I am Zenny! I'm here to be your virtual friend, to chat with you, and to help you find the support and resources you need. Whether you're feeling down and need someone to talk to, or you're looking for information on mental health and therapy, I'm here to listen and assist. So, let's chat and find the help you need!
         st.sidebar.markdown("""
-        <div style="font-family: 'Arial', sans-serif; font-size: 20px; font-style: bold;">
-            Appointment Booking Section
-        </div>
-    """, unsafe_allow_html=True)
+            <div style="font-family: 'Arial', sans-serif; font-size: 20px; font-style: bold;">
+                Appointment Booking Section
+            </div>
+        """, unsafe_allow_html=True)
 
     if option == "Chat":
         if (st.session_state.is_logged_in and st.session_state.user_type == "Patient") or (st.session_state.is_logged_in and st.session_state.user_type == "Admin"):
             agent_chat_prod.main()
         else:
-            st.write("Please log in as a patient to access the chat.")
+            st.write("""<div style="background-color: rgba(255, 255, 255, 0.1); padding: 10px; border-radius: 5px;">
+                        <h3>Please log in as a patient to access the chat.</h3>
+                       </div>
+                    """, unsafe_allow_html=True)
 
     elif option == "Therapist View":
         if (st.session_state.is_logged_in and st.session_state.user_type == "Doctor") or (st.session_state.is_logged_in and st.session_state.user_type == "Admin"):
-            
-
             therapy.main()
         else:
-            st.write("Please log in as Therapist to access this page.")
+            st.write("""<div style="background-color: rgba(255, 255, 255, 0.1); padding: 10px; border-radius: 5px;">
+                        <h3>Please log in as a therapist to access this page.</h3>
+                       </div>
+                    """, unsafe_allow_html=True)
 
     elif option == "Admin Dashboard":
         if st.session_state.is_logged_in and st.session_state.user_type == "Admin":
-            st.write("Admin Dashboard")
+            st.write("""<div style="background-color: rgba(255, 255, 255, 0.1); padding: 10px; border-radius: 5px;">
+                        <h3>Welcome to the Admin Dashboard</h3>
+                       </div>
+                    """, unsafe_allow_html=True)
             dashboard.main()
         else:
-            st.write("Please log in as Admin to access the dashboard.")
+            st.write("""<div style="background-color: rgba(255, 255, 255, 0.1); padding: 10px; border-radius: 5px;">
+                        <h3>Please log in as an admin to access the dashboard.</h3>
+                       </div>
+                    """, unsafe_allow_html=True)
     
     elif option == "Login":
         st.session_state.is_logged_in = login.main()
@@ -57,7 +87,10 @@ def main():
         if st.session_state.is_logged_in:
             dashboard.main()  # Call the main function of the therapist dashboard
         else:
-            st.write("Please log in to access the therapist dashboard.")
+            st.write("""<div style="background-color: rgba(255, 255, 255, 0.1); padding: 10px; border-radius: 5px;">
+                        <h3>Please log in to access the dashboard.</h3>
+                       </div>
+                    """, unsafe_allow_html=True)
 
     else:
         st.session_state.is_logged_in = login.main()
@@ -65,4 +98,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
